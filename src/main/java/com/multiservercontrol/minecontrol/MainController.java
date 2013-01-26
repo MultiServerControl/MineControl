21f74dcdd6fb60a91d3f1f06bf58a2e885afa0e1
F481 <fabi.kcirf@gmail.com>
2013-01-26 20:34:03 +0100

    implemented simple start method

--- src/main/java/com/multiservercontrol/minecontrol/MainController.java
+++ src/main/java/com/multiservercontrol/minecontrol/MainController.java
@@ -1,36 +1,68 @@
 package com.multiservercontrol.minecontrol;
 
+import java.io.IOException;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
 public class MainController {
+
+    private ProcessBuilder builder = new ProcessBuilder("");
+    private Configuration config = null;
+
+    public MainController() {
+	try {
+	    this.config = new PropertiesConfiguration("minecontrol.properties");
+	} catch (ConfigurationException e) {
+	    // TODO log
+	}
+    }
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-	String command = args[0];
+	MainController controller = new MainController();
+	String server = args[0];
+	String command = args[1];
+
 	if (command.equals("start")) {
-	    start();
+	    controller.start(server);
 	} else if (command.equals("stop")) {
-	    stop();
+	    controller.stop();
 	} else if (command.equals("restart")) {
-	    restart();
+	    controller.restart();
 	} else if (command.equals("status")) {
-	    isRunning();
+	    controller.isRunning();
 	}
     }
 
-    public static void start() {
-	System.out.println("start");
+    public void start(String serverId) {
+	System.out.println("Starting server...");
+
+	String startCommand = config.getString("command.start");
+	this.builder.command("screen", "-dmS", serverId, "java", "-jar",
+		"minecraft_server.jar", "nogui");
+	builder.redirectErrorStream(true);
+
+	try {
+	    Process p = builder.start();
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
     }
 
-    public static void stop() {
+    public void stop() {
 	System.out.println("stop");
     }
 
-    public static void restart() {
+    public void restart() {
 	System.out.println("restart");
     }
 
-    public static void isRunning() {
+    public void isRunning() {
 	System.out.println("status");
     }
 }
