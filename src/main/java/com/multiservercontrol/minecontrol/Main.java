4637ebc022fb6e017ea5a0ac9fb48d854145af16
F481 <fabi.kcirf@gmail.com>
2013-02-17 02:21:40 +0100

    refactored entry point main class

---
+++ src/main/java/com/multiservercontrol/minecontrol/Main.java
@@ -0,0 +1,32 @@
+package com.multiservercontrol.minecontrol;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	ServerController controller = new ServerController();
+	ServerMessenger messenger = new ServerMessenger();
+
+	String screenName = args[0];
+	String command = args[1];
+
+	if (command.equals("start")) {
+	    controller.start(screenName);
+	} else if (command.equals("stop")) {
+	    // TODO pass messenger to work with
+	    controller.stop(screenName);
+	} else if (command.equals("restart")) {
+	    controller.restart(screenName);
+	} else if (command.equals("status")) {
+	    controller.isRunning(screenName);
+	} else if (command.equals("pid")) {
+	    controller.getPid(screenName);
+	} else if (command.equals("command")) {
+	    // TODO dev
+	    String serverCommand = args[2];
+	    messenger.sendServerCommand(screenName, serverCommand);
+	}
+    }
+}
