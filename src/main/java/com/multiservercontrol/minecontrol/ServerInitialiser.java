9cd7400fd11426961c1795f657021734a94da2c3
F481 <fabi.kcirf@gmail.com>
2013-06-29 01:01:51 +0200

    check if server file exists

--- src/main/java/com/multiservercontrol/minecontrol/ServerInitialiser.java
+++ src/main/java/com/multiservercontrol/minecontrol/ServerInitialiser.java
@@ -1,6 +1,24 @@
 package com.multiservercontrol.minecontrol;
 
+import org.apache.log4j.Logger;
+
+import java.io.File;
 
 public class ServerInitialiser {
 
+    private static Logger LOGGER = Logger.getLogger(ServerController.class);
+    private final static String CONFIG_FILE_NAME = "minecontrol.properties";
+    private final static String CONFIG_LOGGER_LEVEL = "logger.level";
+    private final static String CONFIG_SHELL_BIN = "shell.bin";
+
+    public boolean serverFileExists(String fileName)
+    {
+        File serverFile = new File(fileName);
+
+        if (serverFile.exists()) {
+            return true;
+        }
+        System.out.println("Der Server existiert nicht!");
+        return false;
+    }
 }
